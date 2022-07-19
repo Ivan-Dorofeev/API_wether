d929812dbf8777b8c571b35f63c00c37c0da6aa1
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-19 11:47:05 +0300

    - Подсушил кода - Добавил верные исключения - Поправил параметры url запроса

--- weather.py
+++ weather.py
@@ -1,11 +1,16 @@
 import requests
 
-cities = ['svo', 'lond232on', 'cherepovets']
-data = {'?M': '', '?1': '', '?n': '', '?T': '', '?Q': '', 'lang': 'ru'}
+cities = ['svo', 'london', 'cherepovets']
+views_params = {'1': '', 'n': '', 'T': '', 'Q': '', 'lang': 'ru'}
 for city in cities:
-    response = requests.get(f"https://wttr.in/{city}", params=data)
+    response = requests.get(f"https://wttr.in/{city}", params=views_params)
+    print(response.url)
     try:
-        if response.raise_for_status() is None:
-            print(response.text)
-    except Exception as exc:
-        print(f'При запросе возникла ошибка: \n{exc}')
+        response.raise_for_status()
+        print(response.text)
+    except requests.exceptions.HTTPError as exc:
+        print(f'Неверно введён адрес запроса: \n{exc}')
+    except requests.exceptions.ConnectionError as exc:
+        print(f'Ошибка подключения: \n{exc}')
+    except requests.exceptions.Timeout as exc:
+        print(f'Долгое ожидание ответа от сервер: \n{exc}')
