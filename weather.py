664e55fc02904bbb6357d9ef100ecf36fbf242cb
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-20 10:02:38 +0300

    - Поправил параметры запроса - Добавил main()

--- weather.py
+++ weather.py
@@ -1,11 +1,17 @@
 import requests
 
-cities = ['svo', 'london', 'cherepovets']
-views_params = {'1nTQ': '', 'lang': 'ru'}
-for city in cities:
-    response = requests.get(f"https://wttr.in/{city}", params=views_params)
-    try:
-        response.raise_for_status()
-        print(response.text)
-    except requests.exceptions.HTTPError as exc:
-        print(f'Неверно введён адрес запроса: \n{exc}')
+
+def main():
+    cities = ['svo', 'london', 'cherepovets']
+    cities_view_params = {'nTqM': '', 'lang': 'ru'}
+    for city in cities:
+        response = requests.get(f"https://wttr.in/{city}", params=cities_view_params)
+        try:
+            response.raise_for_status()
+            print(response.text)
+        except requests.exceptions.HTTPError as exc:
+            print(f'Неверно введён адрес запроса: \n{exc}')
+
+
+if __name__ == '__main__':
+    main()
